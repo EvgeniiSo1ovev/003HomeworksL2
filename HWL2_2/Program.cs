4f072f7635d46530d464244ede1ea9cded51a9bf
Evgenii Solovev <krekersol@mail.ru>
2022-12-02 19:41:00 +0900

    Выполнена вторая задача

---
+++ HWL2_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа. Не использовать строки для расчета
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine($"Случайное трехзначное число: {number}");
+int number1 = number / 100;
+int number3 = number % 10;
+number = number1*10 + number3;
+System.Console.WriteLine($"Полученное число: {number}");
